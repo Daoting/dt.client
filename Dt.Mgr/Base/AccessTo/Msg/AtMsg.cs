8e8884747027b7afc4e8d771d5ac1848287b2d9c
daoting <daoting@outlook.com>
2023-12-11 10:36:09 +0800

    初始文件

---
+++ Dt.Mgr/Base/AccessTo/Msg/AtMsg.cs
@@ -0,0 +1,13 @@
+﻿namespace Dt.Mgr
+{
+    /// <summary>
+    /// msg服务的数据访问
+    /// </summary>
+    public partial class AtMsg : DataAccess<AtMsg.Info>
+    {
+        public class Info : AccessInfo
+        {
+            public override string Name => "msg";
+        }
+    }
+}
